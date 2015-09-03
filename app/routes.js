1f4f5bb974bfabc333a83d1754961bd0e06a1081
Jeremy Ward <jerskiward@gmail.com>
2015-09-03 13:28:14 +0000

    updated routes and package

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,14 @@
 //===app/routes.js
 module.exports=function(app){
-
-    //===Domino Route============
-        app.get("/domino", function(req, res){
-          res.render('domino.ejs');
-        });
-        
     //===Main route==============
         app.get("/",function(req, res) {
            res.redirect("/domino") 
         });
     
+    //===Domino Route============
+        app.get("/domino", function(req, res){
+          res.render('domino.ejs');
+        });
+        
+
 }
